898e42005905c9f1ac7c55b7e9642336c8f092b0
Martin Polak <Martin Polak@itb.int>
2024-02-29 12:29:46 +0100

    Den Lifecycle von Doctrine-Objekten verwalten

---
+++ migrations/Version20240229105416.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240229105416 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE conference ALTER city SET NOT NULL');
+        $this->addSql('ALTER TABLE conference ALTER slug SET NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE conference ALTER city DROP NOT NULL');
+        $this->addSql('ALTER TABLE conference ALTER slug DROP NOT NULL');
+    }
+}
