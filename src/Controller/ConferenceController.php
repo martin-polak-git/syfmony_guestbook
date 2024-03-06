2b095fb974bd71b9c7ca1748b0db020dd21d7ac9
Martin Polak <martinpolak.mail@gmail.com>
2024-03-06 17:14:05 +0100

    Spam mit Hilfe einer API verhindern

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,7 @@ use App\Form\CommentType;
 use App\Entity\Conference;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -36,12 +37,14 @@ class ConferenceController extends AbstractController
         Request $request,
         Conference $conference,
         CommentRepository $commentRepository,
+        SpamChecker $spamChecker,
         #[Autowire('%photo_dir%')] string $photoDir,
     ): Response
     {
 
         $comment = new Comment();
         $form = $this->createForm(CommentType::class, $comment);
+
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $comment->setConference($conference);
@@ -52,6 +55,20 @@ class ConferenceController extends AbstractController
             }
         
             $this->entityManager->persist($comment);
+
+            
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+            ];
+
+            if (2 === $spamChecker->getSpamScore($comment, $context)) {
+                throw new \RuntimeException('Blatant spam, go away!');
+            }
+        
+
             $this->entityManager->flush();
         
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
