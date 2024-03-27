cc022b46129b17a9e6f502dc8d1a1c7c6d9eea8a
Martin Polak <Martin Polak@itb.int>
2024-03-27 14:18:25 +0100

    Bilder skalieren

--- assets/app.js
+++ assets/app.js
@@ -11,6 +11,6 @@ import 'bootstrap';
 import bsCustomFileInput from 'bs-custom-file-input';
 
 // start the Stimulus application
-import './bootstrap';
+//import './bootstrap';
 
 bsCustomFileInput.init();
